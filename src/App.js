993b61efe8691a5599953efa4b6891f997e483e1
becky <rlcampelli@gmail.com>
2019-03-23 10:49:28 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fname: '',
+      lname: '',
+      email: '',
+    };
+  }
+
+  onChange = (e) => {
+    /*
+      Because we named the inputs to match their
+      corresponding values in state, it's
+      super easy to update the state
+    */
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    // get our form data out of state
+    const { fname, lname, email } = this.state;
+
+    axios.post('/', { fname, lname, email })
+      .then((result) => {
+        //access the results here....
+      });
+  }
+
   render() {
+    const { fname, lname, email } = this.state;
+    console.log("inside render ", this.state);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <form onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          name="fname"
+          value={fname}
+          onChange={this.onChange}
+        />
+        <input
+          type="text"
+          name="lname"
+          value={lname}
+          onChange={this.onChange}
+        />
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={this.onChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
     );
   }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -6,4 +6,10 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+
+  ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+  );
+
 });
